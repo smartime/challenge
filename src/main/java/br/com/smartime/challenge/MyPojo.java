a3c13965f25f896e91ee0bcf7aee8258916dbb46
Alisson Henrique <alissonhenrique001@hotmail.com.br>
2019-11-09 01:18:52 -0300

    POJO = Plain Old Java Object (Velho e Simples Objeto Java)

---
+++ src/main/java/br/com/smartime/challenge/MyPojo.java
@@ -0,0 +1,36 @@
+
+public class MyPojo {
+
+    public int id;
+    public String name;
+    public float price;
+    public String tags[];
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public String[] getTags() {
+        return tags;
+    }
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    
+}
+
+//Author: Alisson
